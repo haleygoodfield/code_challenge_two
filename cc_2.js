1413ba75712394ab26efe8eab349998ed31f0742
Haley Goodfield <haleygoodfield@usf.edu>
2025-01-23 17:47:43 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,15 @@
+// Task 1 - Working with Arrays
+// Declare an array products using let and initialize it with at least five product names.
+let products = ["Shampoo", "Lotion", "Perfume", "Conditioner", "Body Wash"];
+
+// Add a new product to the array.
+products.push("Face Wash");
+
+// Remove the last product from the array.
+products.pop();
+
+//The new product was removed from the array, as it was the last product 
+
+// Log the updated product list to the console.
+console.log("Updated Product List:", products)
+
